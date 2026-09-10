0d026cbd9bdfba5d2545e3129f997477a9c561b3
Diplom <kirito0821@mail.ru>
2026-09-10 16:27:22 +0500

    Task1

---
+++ Tyuiu.VekshinGF.Sprint0.Task1.V1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.VekshinGF.Sprint0.Task1.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Григорий";
+            int age = 19;
+                Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет !!!");
+            Console.ReadKey();
+        }
+    }
+}
