673f38177b85df28591be1b4bfa7cb8cf6e098b9
Diplom <kirito0821@mail.ru>
2026-09-14 13:43:51 +0500

    Task6

---
+++ Tyuiu.VekshinGF.Sprint0.Task6.V1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.VekshinGF.Sprint0.Task6.V1.Lib;
+
+namespace Tyuiu.VekshinGF.Sprint0.Task6.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма Элементов массива = " + DataService.AdditionArray(arraynums) );
+
+            Console.WriteLine("Разность Элементов массива = " + DataService.SubtractionArray(arraynums));
+
+            Console.WriteLine("Произведение Элементов массива = " + DataService.MultiplicationArray(arraynums));
+            
+            Console.ReadKey();
+        }
+    }
+}
