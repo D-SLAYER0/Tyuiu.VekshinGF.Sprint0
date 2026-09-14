34d360f20de3a1db978716d77db6e9b59cc7053c
Diplom <kirito0821@mail.ru>
2026-09-14 11:14:24 +0500

    Task2

---
+++ Tyuiu.VekshinGF.Sprint0.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.VekshinGF.Sprint0.Task2.V1.Lib;
+
+namespace Tyuiu.VekshinGF.Sprint0.Task2.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Григорий";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Григорий", res);
+        }
+    }
+}
---
+++ Tyuiu.VekshinGF.Sprint0.V1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.VekshinGF.Sprint0.Task2.V1.Lib;
+
+namespace Tyuiu.VekshinGF.Sprint0.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Григорий"));
+            Console.ReadKey();
+        }
+    }
+}
