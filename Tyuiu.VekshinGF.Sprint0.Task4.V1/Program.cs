fc2e77948380826ed0a0531e3c214cfef5349dc2
Diplom <kirito0821@mail.ru>
2026-09-14 12:41:13 +0500

    Task4

---
+++ Tyuiu.VekshinGF.Sprint0.Task4.V1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.VekshinGF.Sprint0.Task4.V1.Lib;
+
+namespace Tyuiu.VekshinGF.Sprint0.Task4.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1,5));
+
+            Console.WriteLine(DataService.Subraction(1, 5));
+
+            Console.WriteLine(DataService.Multiplication(1, 5));
+
+            Console.WriteLine(DataService.Division(1, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
